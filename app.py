34a4bda584e8005ddfef996781d87cc7c2231051
ravi <ravicld28@gmail.com>
2025-04-18 22:17:31 +0530

    app.py updated by developer1

---
+++ app.py
@@ -0,0 +1,9 @@
+def lambda_handler(event, context):
+    client = boot3.client('ec2')
+    response = client.run_instance(
+            ImageId='ami-0614680123427b75e',
+            InstanceType='t2.micro',
+            KeyName='mykeymumbai',
+            MaxCount=2,
+            MinCount=1
+            )
